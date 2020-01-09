0719ef13b58e65d0ab255c7a6458c33119683b34
datalessuk <32886171+datalessuk@users.noreply.github.com>
2020-01-09 17:23:16 +0000

    Added full sign up page

--- app/src/main/java/mark/harrison/finalproject/MainActivity.java
+++ app/src/main/java/mark/harrison/finalproject/MainActivity.java
@@ -2,13 +2,25 @@ package mark.harrison.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onClick(View v){
+        Intent openSignUp = new Intent(getApplicationContext(),signUpActivity.class);
+        startActivity(openSignUp);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
+
 }
