9069ec2d20189da23cf6e80daa5473c7b990e73a
datalessuk <32886171+datalessuk@users.noreply.github.com>
2020-01-10 16:24:21 +0000

    starting to add Firebase

--- app/src/main/java/mark/harrison/finalproject/signUpActivity.java
+++ app/src/main/java/mark/harrison/finalproject/signUpActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class signUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
+    
+
+
 }
