4e9a17ba42aac8395cf33176afa705a4cec8c741
datalessuk <32886171+datalessuk@users.noreply.github.com>
2020-01-18 14:02:10 +0000

    Added Class for all user validation need to add name

---
+++ app/src/main/java/mark/harrison/finalproject/inputValidation.java
@@ -0,0 +1,85 @@
+package mark.harrison.finalproject;
+
+import android.util.Patterns;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class inputValidation {
+
+    private static inputValidation sInstance;
+
+
+
+    public static inputValidation getInstance(){
+        if(sInstance == null){
+            sInstance = new inputValidation();
+        }
+    return sInstance;
+    }
+
+
+
+
+    //Method to validate the email
+    public boolean emailCheck(String pEmail){
+
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+
+        return pattern.matcher(pEmail).matches();
+
+    }
+
+    //Validate the Password
+
+    public boolean passwordChecker(String pPassWord){
+        Pattern special = Pattern.compile ("[!@#$£%&*()_+=|<>?{}\\[\\]~-]");
+        if(pPassWord.length() >6){
+            Matcher hasSpecial = special.matcher(pPassWord);
+
+            return hasSpecial.find();
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public int calculateAge(String pDOBString){
+
+        Date date = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            date = sdf.parse(pDOBString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        if(date == null) return 0;
+
+        Calendar dob = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
+
+        dob.setTime(date);
+
+        int year = dob.get(Calendar.YEAR);
+        int month = dob.get(Calendar.MONTH);
+        int day = dob.get(Calendar.DAY_OF_MONTH);
+
+        dob.set(year, month+1, day);
+
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+
+        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)){
+            age--;
+        }
+
+
+        return age;
+    }
+
+}
--- app/src/main/java/mark/harrison/finalproject/signUpActivity.java
+++ app/src/main/java/mark/harrison/finalproject/signUpActivity.java
@@ -3,26 +3,18 @@ package mark.harrison.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
-import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import org.w3c.dom.Text;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -33,6 +25,7 @@ import java.util.regex.Pattern;
 
 
 public class signUpActivity extends AppCompatActivity {
+
 
     private FirebaseAuth mAuth;
     private TextView mDisplayDate;
@@ -93,7 +86,7 @@ public class signUpActivity extends AppCompatActivity {
                 date = month + "/" + day + "/" + year;
                 mDisplayDate.setText(date);
 
-                int userAge = calculateAge(date);
+                int userAge = inputValidation.getInstance().calculateAge(date);
 
                 if(userAge < 18){
                     mCreateAccountbutton.setEnabled(false);
@@ -111,78 +104,25 @@ public class signUpActivity extends AppCompatActivity {
         mCreateAccountbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            if(!passwordChecker(mpasswordInput.getText().toString())){
+            if(!inputValidation.getInstance().passwordChecker(mpasswordInput.getText().toString())){
 
                 mpasswordInput.setError("Sorry your password has to be longer than 6 with one special character"); // Working
             }
-            if(!emailCheck(mEamilImput.getText().toString())){
+            if(!inputValidation.getInstance().emailCheck(mEamilImput.getText().toString())){
                 mEamilImput.setError("Sorry that is not a valid Email Address");
             }
 
 
             }
+
+
+
         });
 
     //End of input for date
 
     }
-    //To make sure the passworld is longer than six chars and has one speical charicater
 
-    public boolean passwordChecker(String pPassWord){
-        Pattern special = Pattern.compile ("[!@#$£%&*()_+=|<>?{}\\[\\]~-]");
-        if(pPassWord.length() >6){
-            Matcher hasSpecial = special.matcher(pPassWord);
-
-            return hasSpecial.find();
-        }
-        else {
-            return false;
-        }
-
-    }
-
-    //Works out the user ages
-
-    private int calculateAge(String pDOBString){
-
-        Date date = null;
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        try {
-            date = sdf.parse(pDOBString);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        if(date == null) return 0;
-
-        Calendar dob = Calendar.getInstance();
-        Calendar today = Calendar.getInstance();
-
-        dob.setTime(date);
-
-        int year = dob.get(Calendar.YEAR);
-        int month = dob.get(Calendar.MONTH);
-        int day = dob.get(Calendar.DAY_OF_MONTH);
-
-        dob.set(year, month+1, day);
-
-        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
-
-        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)){
-            age--;
-        }
-
-
-        return age;
-    }
-    //Email Validation
-    private boolean emailCheck(String pEmail){
-
-        Pattern pattern = Patterns.EMAIL_ADDRESS;
-
-        return pattern.matcher(pEmail).matches();
-
-    }
 
 
 }
