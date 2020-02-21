98afd72f25fe2d704a1bfc03588ed00454720df4
datalessuk <32886171+datalessuk@users.noreply.github.com>
2020-02-21 17:13:30 +0000

    adding beer info page

---
+++ app/src/main/java/mark/harrison/finalproject/beerInformation.java
@@ -0,0 +1,26 @@
+package mark.harrison.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class beerInformation extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beer_information);
+
+
+
+
+    }
+
+
+}
--- app/src/main/java/mark/harrison/finalproject/homeScreen.java
+++ app/src/main/java/mark/harrison/finalproject/homeScreen.java
@@ -2,16 +2,33 @@ package mark.harrison.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class homeScreen extends AppCompatActivity {
 
+    Button mBeerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        mBeerButton = (Button)findViewById(R.id.beerInfoButton);
+        mBeerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),beerInformation.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
 
     }
+
+
+
+
 }
