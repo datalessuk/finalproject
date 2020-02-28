a7f076b5ccbf2c5a03c4d9c0c98b666f3cfdabad
datalessuk <32886171+datalessuk@users.noreply.github.com>
2020-02-28 16:19:58 +0000

    adding beer info page

---
+++ app/src/main/java/mark/harrison/finalproject/createBeer.java
@@ -0,0 +1,14 @@
+package mark.harrison.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createBeer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_beer);
+    }
+}
