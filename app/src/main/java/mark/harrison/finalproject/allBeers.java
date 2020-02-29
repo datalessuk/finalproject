f2fe5fc827bb20b178646de04da41680e5bc6a67
datalessuk <32886171+datalessuk@users.noreply.github.com>
2020-02-29 16:51:38 +0000

    Adding Beer

--- app/src/main/java/mark/harrison/finalproject/allBeers.java
+++ app/src/main/java/mark/harrison/finalproject/allBeers.java
@@ -1,14 +1,71 @@
 package mark.harrison.finalproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class allBeers extends AppCompatActivity {
+
+
+    private ListView listView;
+
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    ArrayList<String> mAllBeers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_beers);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Beers");
+
+        listView =(ListView)findViewById(R.id.allBeersList);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mAllBeers);
+        listView.setAdapter(arrayAdapter);
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String value = dataSnapshot.getValue(String.class);
+                mAllBeers.add(value);
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
