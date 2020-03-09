69475394c52a9dbc6b3783b65a8c9a98b048234d
datalessuk <32886171+datalessuk@users.noreply.github.com>
2020-03-09 15:12:21 +0000

    Can View beer now Redesign UI next

--- app/src/main/java/mark/harrison/finalproject/beerInformation.java
+++ app/src/main/java/mark/harrison/finalproject/beerInformation.java
@@ -1,6 +1,7 @@
 package mark.harrison.finalproject;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.vision.barcode.Barcode;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,6 +20,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class beerInformation extends AppCompatActivity {
 
@@ -49,13 +54,69 @@ public class beerInformation extends AppCompatActivity {
         DatabaseReference zone1NameRef = zone1Ref.child("mName");
         DatabaseReference zone2 =  zone1Ref.child("mBrewery");
 
+        //final ArrayList my = new ArrayList();
+
+
 
 
         setContentView(R.layout.activity_beer_information);
 
         mBeername = (TextView)findViewById(R.id.BeerNameText);
         mBeerBrewery = (TextView) findViewById(R.id.BreweryText);
-        zone1NameRef.addValueEventListener(new ValueEventListener() {
+
+        zone1Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    //Beers beers = ds.getValue(Beers.class);
+                    //mBeername.setText(beers.getmName().toString());
+                   //String name = ds.child("mName").getValue(String.class);
+                    Beers beers = dataSnapshot.getValue(Beers.class);
+                    String beerclass = beers.getmName();
+                    String b = beers.getmBrewery();
+                    mBeername.setText(beerclass);
+                    mBeerBrewery.setText(b);
+                    //my.add(beers);
+                    //String beername = ds.child("mBarcode").getValue(String.class);
+                    ////mBeername.setText(beername);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //zone1Ref.addChildEventListener(new ChildEventListener() {
+         /*   @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Beers beers = dataSnapshot.getValue(Beers.class);
+                mBeername.setText(beers.getmName().toString());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });*/
+        /* zone1NameRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //String beerName = dataSnapshot.child("5010038460160").getValue().toString();
@@ -83,7 +144,7 @@ public class beerInformation extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        });
+        });*/
 
 
 
